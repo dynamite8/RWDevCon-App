b06a1f00a6c8e1006ff00e8661088cff684a627b
Jenny Chang Ho <Jchangho@grubhub.com>
2018-04-07 01:44:48 -0400

    Imported Koloda lib

---
+++ Podfile
@@ -0,0 +1,35 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RWDevCon' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RWDevCon
+  pod 'Koloda', '~> 4.3.1'
+
+end
+
+target 'Today' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Today
+
+end
+
+target 'Watch' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Watch
+
+end
+
+target 'Watch Extension' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Watch Extension
+
+end
--- RWDevCon/View Controllers/ExploreSessionsViewController.swift
+++ RWDevCon/View Controllers/ExploreSessionsViewController.swift
@@ -7,9 +7,62 @@
 //
 
 import UIKit
+import Koloda
 
 class ExploreSessionsViewController : UIViewController {
-  override func viewDidLoad() {
-    view.backgroundColor = .blue
+  
+  private var kolodaView: KolodaView!
+  private lazy var conference = ConferenceManager.current!
+  private var displayedSessions: [[Session]] {
+    return conference.sessions
   }
+  
+  override func viewDidLoad() {
+    view.backgroundColor = .black
+    kolodaView = KolodaView(frame: view.frame)
+    view.addSubview(kolodaView)
+    kolodaView.dataSource = self
+    kolodaView.delegate = self
+  }
+  
+  private func updateForConference(_ conference: Conference) {
+    self.conference = conference
+  }
+}
+
+// MARK: - ExploreSessionsViewController
+
+extension ExploreSessionsViewController : KolodaViewDelegate {
+  func kolodaDidRunOutOfCards(_ koloda: KolodaView) {
+    koloda.reloadData()
+  }
+  
+  func koloda(_ koloda: KolodaView, didSelectCardAt index: Int) {
+    // TODO: share link to view the kitura info api
+//    UIApplication.shared.openURL(URL(string: "https://yalantis.com/")!)
+  }
+}
+
+// MARK: - KolodaViewDataSource
+
+extension ExploreSessionsViewController: KolodaViewDataSource {
+  
+  func kolodaNumberOfCards(_ koloda:KolodaView) -> Int {
+    return displayedSessions.count
+  }
+  
+  func kolodaSpeedThatCardShouldDrag(_ koloda: KolodaView) -> DragSpeed {
+    return .fast
+  }
+  
+  func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+    guard let speaker = displayedSessions[index].first?.speakers?.first else { return UIView() }
+    let speakerView = SpeakerView(speaker: speaker)
+    speakerView.backgroundColor = .white
+    return speakerView
+  }
+  
+//  func koloda(_ koloda: KolodaView, viewForCardOverlayAt index: Int) -> OverlayView? {
+//    return Bundle.main.loadNibNamed("OverlayView", owner: self, options: nil)[0] as? OverlayView
+//  }
 }
